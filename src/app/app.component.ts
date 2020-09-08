9b18389ea648e8cbfad63ab62bbcde2dc2b5dce4
pierremrcr <pierremercier4@gmail.com>
2020-09-08 16:38:11 +0200

    AppareilService created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {callbackify} from 'util';
+import {AppareilService} from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-projet-angular';
+
+export class AppComponent implements OnInit {
+  isAuth = false;
+
+  appareils: any[];
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+
+  constructor(private appareilService: AppareilService){
+    setTimeout(
+       () => {
+        this.isAuth = true;
+    }, 4000
+    );
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponentComponent } from './mon-premier-component/mon-premier-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import {AppareilService} from './services/appareil.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponentComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
